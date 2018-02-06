9fe42c7e9563c24089f6a88e8d5d6df742f032bf
Mark Lee <electronjs@lazymalevolence.com>
2018-02-05 20:27:53 -0800

    Test constructor without args

--- test/DestroyerOfModules_spec.ts
+++ test/DestroyerOfModules_spec.ts
@@ -10,71 +10,81 @@ async function moduleExists(modulePath: string) {
 }
 
 describe('DestroyerOfModules', () => {
-  let tempDir: string;
-  let tempPackageDir: string;
-  let nodeModulesPath: string;
+  describe('no rootDirectory or walker', () => {
+    const creator = () => new galactus.DestroyerOfModules({});
 
-  beforeEach(async () => {
-    tempDir = tempy.directory();
-    tempPackageDir = path.join(tempDir, 'package');
-    nodeModulesPath = path.join(tempPackageDir, 'node_modules');
-
-    await fs.copy(path.join(__dirname, 'fixtures', 'package'), tempPackageDir);
-    await fs.rename(path.join(tempPackageDir, '_node_modules'), nodeModulesPath);
+    it('should throw an error', () => {
+      expect(creator).to.throw(/Must either provide rootDirectory or walker argument/);
+    });
   });
 
-  describe('rootDirectory only specified', () => {
+  describe('valid package', () => {
+    let tempDir: string;
+    let tempPackageDir: string;
+    let nodeModulesPath: string;
+
     beforeEach(async () => {
-      const destroyer = new galactus.DestroyerOfModules({
-        rootDirectory: tempPackageDir
-      });
-      await destroyer.destroy();
+      tempDir = tempy.directory();
+      tempPackageDir = path.join(tempDir, 'package');
+      nodeModulesPath = path.join(tempPackageDir, 'node_modules');
+
+      await fs.copy(path.join(__dirname, 'fixtures', 'package'), tempPackageDir);
+      await fs.rename(path.join(tempPackageDir, '_node_modules'), nodeModulesPath);
     });
 
-    describe('direct dependencies', () => {
-      it('keeps production dependencies', async () => {
-        expect(await moduleExists(path.join(nodeModulesPath, 'test-prod'))).to.be.true;
+    describe('rootDirectory only specified', () => {
+      beforeEach(async () => {
+        const destroyer = new galactus.DestroyerOfModules({
+          rootDirectory: tempPackageDir
+        });
+        await destroyer.destroy();
       });
 
-      it('keeps optional dependencies', async () => {
-        expect(await moduleExists(path.join(nodeModulesPath, 'test-optional'))).to.be.true;
+      describe('direct dependencies', () => {
+        it('keeps production dependencies', async () => {
+          expect(await moduleExists(path.join(nodeModulesPath, 'test-prod'))).to.be.true;
+        });
+
+        it('keeps optional dependencies', async () => {
+          expect(await moduleExists(path.join(nodeModulesPath, 'test-optional'))).to.be.true;
+        });
+
+        it('prunes devDependencies', async () => {
+          expect(await moduleExists(path.join(nodeModulesPath, 'test-dev'))).to.be.false;
+        });
       });
 
-      it('prunes devDependencies', async () => {
-        expect(await moduleExists(path.join(nodeModulesPath, 'test-dev'))).to.be.false;
+      describe('indirect dependencies', () => {
+        it('keeps production dependencies', async () => {
+          expect(await moduleExists(path.join(nodeModulesPath, 'dep-prod'))).to.be.true;
+        });
+
+        it('keeps optional dependencies', async () => {
+          expect(await moduleExists(path.join(nodeModulesPath, 'dep-optional'))).to.be.true;
+        });
+
+        it('prunes devDependencies', async () => {
+          expect(await moduleExists(path.join(nodeModulesPath, 'dep-dev'))).to.be.false;
+        });
+      });
+
+      describe('scoped dependencies', () => {
+        it('keeps production dependencies', async () => {
+          expect(await moduleExists(path.join(nodeModulesPath, '@scoped/scoped-prod'))).to.be.true;
+        });
+
+        it('keeps optional dependencies', async () => {
+          expect(await moduleExists(path.join(nodeModulesPath, '@scoped/scoped-optional'))).to.be.true;
+        });
+
+        it('prunes devDependencies', async () => {
+          expect(await moduleExists(path.join(nodeModulesPath, '@scoped/scoped-dev'))).to.be.false;
+        });
       });
     });
 
-    describe('indirect dependencies', () => {
-      it('keeps production dependencies', async () => {
-        expect(await moduleExists(path.join(nodeModulesPath, 'dep-prod'))).to.be.true;
-      });
-
-      it('keeps optional dependencies', async () => {
-        expect(await moduleExists(path.join(nodeModulesPath, 'dep-optional'))).to.be.true;
-      });
-
-      it('prunes devDependencies', async () => {
-        expect(await moduleExists(path.join(nodeModulesPath, 'dep-dev'))).to.be.false;
-      });
+    afterEach(async () => {
+      await fs.remove(tempDir);
     });
-
-    describe('scoped dependencies', () => {
-      it('keeps production dependencies', async () => {
-        expect(await moduleExists(path.join(nodeModulesPath, '@scoped/scoped-prod'))).to.be.true;
-      });
-
-      it('keeps optional dependencies', async () => {
-        expect(await moduleExists(path.join(nodeModulesPath, '@scoped/scoped-optional'))).to.be.true;
-      });
-
-      it('prunes devDependencies', async () => {
-        expect(await moduleExists(path.join(nodeModulesPath, '@scoped/scoped-dev'))).to.be.false;
-      });
-    });
-  });
-
-  afterEach(async () => {
-    await fs.remove(tempDir);
   });
 });
